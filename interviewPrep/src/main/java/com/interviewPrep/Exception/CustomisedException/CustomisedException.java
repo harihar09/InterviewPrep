3e1775b23ae4fa7e52ab9a44f518917fd8aedaae
harish5058 <hariharsangale@gmail.com>
2023-06-06 17:33:54 +0530

    CustomisedException

--- interviewPrep/src/main/java/com/interviewPrep/Exception/CustomisedException/CustomisedException.java
+++ interviewPrep/src/main/java/com/interviewPrep/Exception/CustomisedException/CustomisedException.java
@@ -3,5 +3,12 @@ package com.interviewPrep.Exception.CustomisedException;
 public class CustomisedException extends  RuntimeException{
     public CustomisedException(String message){
         super(message);
+        // this message would be passed to parent class constructor i.e. RuntimeException
+        // and then to Exception constructor and at last to Throwable class.
+        // printStack() is available in Throwable class which will be called
+        // by JVM default exception handler
     }
 }
+
+
+
